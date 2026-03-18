50db6f2531399e4a50928797ad9547d902f8f5f8
Janusz <gleaken@gmail.com>
2026-03-18 22:50:01 +0100

    movement speed

--- scripts/Components/MovementComponent.cs
+++ scripts/Components/MovementComponent.cs
@@ -19,7 +19,6 @@ public partial class MovementComponent : Node
 
 	public override void _Process(double delta)
 	{
-		var velocity = new Vector2(0, _controlledObject.Velocity.Y);
-		_controlledObject.Velocity = new Vector2(_inputComponent.Direction.X * 50, _controlledObject.Velocity.Y);
+		_controlledObject.Velocity = new Vector2(_inputComponent.Direction.X * 100, _controlledObject.Velocity.Y);
 	}
 }
