dc1dd0800d6263fcfea1e355acca53f52c94136c
Janusz <gleaken@gmail.com>
2026-03-22 09:40:25 +0100

    paralax backgroud

--- scripts/Camera2d.cs
+++ scripts/Camera2d.cs
@@ -19,7 +19,7 @@ public partial class Camera2d : Camera2D
 		if(Mathf.Abs(_follow.Position.X - Position.X) > _tweenDistance || Mathf.Abs(_follow.Position.Y - Position.Y) > (_tweenDistance*.7))
 		{
 			var tween = GetTree().CreateTween();
-			tween.TweenProperty(this, "position", new Vector2(_follow.Position.X, _follow.Position.Y), 0.5f);
+			tween.TweenProperty(this, "position", new Vector2(_follow.Position.X, Position.Y), 0.5f);
 			// GD.Print("Camera2d: " + _follow.Position.ToString());
 			// Position = new Vector2(_follow.Position.X, _follow.Position.Y);
 		}
