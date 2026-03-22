c301896544ff8f519ea69a3df6f9d695bacb287c
Janusz <gleaken@gmail.com>
2026-03-22 13:15:16 +0100

    layers cleanup

--- scenes/city_enemies/CitySkater.cs
+++ scenes/city_enemies/CitySkater.cs
@@ -2,9 +2,6 @@ using Godot;
 
 public partial class CitySkater : CharacterBody2D, IEnemy
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
-	
 	private AnimatedSprite2D _sprite;
 	
 	public override void _Ready()
@@ -27,12 +24,7 @@ public partial class CitySkater : CharacterBody2D, IEnemy
 		Velocity = velocity;
 		MoveAndSlide();
 	}
-
-	public void OnInputEvent(Node body, InputEvent @event, int shapeIdx)
-	{
-		GD.Print("InputEvent");
-	}
-
+	
 	public void Hit(int damage)
 	{
 		GD.Print($"Hit for {damage} damage");
